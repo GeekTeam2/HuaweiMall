72239ae887582e452558ada16997aa705c6682e2
LoneYin <619247724@qq.com>
2017-10-26 20:32:57 +0800

    111

--- js/module-infoLoad.js
+++ js/module-infoLoad.js
@@ -4,21 +4,34 @@ define(function(){
    
    
    var func = function(){
+
+      loadmain($("#mobile"),"../servlet/ProductServlet?method=mobileShowInfo");
+      loadmain($("#laptop"),"../servlet/ProductServlet?method=computerShowInfo");
+      loadmain($("#pad"),"../servlet/ProductServlet?method=padShowInfo");
+      loadmain($("#watch"),"../servlet/ProductServlet?method=wearShowInfo");
+      loadmain($("#smartHome").find(".part-content"),"../servlet/ProductServlet?method=homeShowInfo");
+      loadpart($("#smartHome").find(".scroll-box"), "../servlet/ProductServlet?method=qualityDescShowInfo");
+
+      loadmain($("#hotsellpart").find(".part-content"),"../servlet/ProductServlet?method=hotPartsShowInfo");
+      loadpart($("#smartHome").find(".scroll-box"), "../servlet/ProductServlet?method=qualityShowInfo");
+
+      loadmain($("#brandpart").find(".part-content"),"../servlet/ProductServlet?method=brandShowInfo");
+      loadpart($("#smartHome").find(".scroll-box"), "../servlet/ProductServlet?method=mobileShowInfo");
+
+      loadmain($(".hotsell"),"../servlet/ProductServlet?method=showInfo");
+      loadpart($(".recommend"),"../servlet/ProductServlet?method= qualityShowInfo");
       
-
-      loadmain($("#mobile"),"servlet/ProductServlet?method=mobileShowInfo");
-
       function loadmain(node,url){
          $.ajax({
             type: "POST",
-            data: {sort:hotsell},
+            data: {sort:"hotsell"},
             url: url,
-            success: function (node,response) {
+            success: function (response) {
                var count = 0;
                var data = JSON.parse(response);
                node.find(".grid-items").each(function(index, element){
                   if(index >= 1){
-                     $(this).find(".grid-img").find("img").attr("src", data[count].image);
+                     $(this).find(".grid-img").find("img").attr("src", "../" + data[count].image);
                      $(this).find(".grid-title").html(data[count].name);
                      $(this).find(".grid-desc").html(data[count].desca);
                      $(this).find(".grid-price").html(data[count].price);
@@ -27,7 +40,27 @@ define(function(){
                   }
                })
 
-               node.find(".grid-items").eq(0).find("img").attr("src",data[count + 1].image);
+               node.find(".grid-items").eq(0).find("img").attr("src", "../" + data[count + 1].image);
+            }
+         });
+      }
+
+      function loadpart(node,url){
+         $.ajax({
+            type: "POST",
+            data: {sort:"hotsell"},
+            url: url,
+            success: function (response) {
+               var count = 0;
+               var data = JSON.parse(response);
+               node.find(".grid-items").each(function(){
+                  $(this).find(".grid-img").find("img").attr("src", "../" + data[count].image);
+                  $(this).find(".grid-title").html(data[count].name);
+                  $(this).find(".grid-desc").html(data[count].desca);
+                  $(this).find(".grid-price").html(data[count].price);
+                  $(this).find(".grid-tips").find("i").addClass(data[count].tag);
+                  count++;
+               })
             }
          });
       }
