75a941d24ca833c677c01807343447110c7afdf1
LoneYin <619247724@qq.com>
2017-10-27 09:00:55 +0800

    小改动

--- js/module-loginStatus.js
+++ js/module-loginStatus.js
@@ -2,8 +2,17 @@ define(['jquery','cookie'], function($){
    
    var func = function(){
       if($.cookie("login-status")){
-         alert("已登录");
+         var data =  JSON.parse($.cookie("login-status"));
+         if(data.name != " "){
+            $("#unlogin-status").css("display","none");
+            $("#login-status").css("display","block");
+            $("#user-login").html("你好，" + data.name +"&nbsp;&nbsp;");
+         }
+      }else{
+         alert(1);
       }
+      
+
    }   
    
    return {
