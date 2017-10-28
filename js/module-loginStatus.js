43f44ccfcdc65425a7e23c9932b8e8e0cb1b5d94
zxm-china <32837976+zxm-china@users.noreply.github.com>
2017-10-28 09:22:12 +0800

    1234

--- js/module-loginStatus.js
+++ js/module-loginStatus.js
@@ -2,16 +2,32 @@ define(['jquery','cookie'], function($){
    
    var func = function(){
       if($.cookie("login-status")){
-         var data =  JSON.parse($.cookie("login-status"));
-         if(data.name != " "){
+    	 var data = $.cookie("login-status");
+         if(data != ""){
             $("#unlogin-status").css("display","none");
             $("#login-status").css("display","block");
-            $("#user-login").html("你好，" + data.name +"&nbsp;&nbsp;");
+            $("#user-login").html("你好，" + data);
+         }else{
+        	alert("请重新登录");
          }
+         
       }else{
-         alert(1);
+         console.log("请登录");
       }
       
+      $("#btn-logout").on("click",function(){
+    	  $("#unlogin-status").css("display","block");
+          $("#login-status").css("display","none");
+          $.cookie("login-status","");
+//          $.ajax({
+//        	  url:"",
+//        	  type:"POST",
+//        	  data:"logout",
+//        	  success:function(res){
+//        		  alert(1);
+//        	  }
+//          })
+      })
 
    }   
    
